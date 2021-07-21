f89d2c5a0666678cd34bf663189c10476d3d1450
Cosmin Ciobanu <ciobanu.cosmin@yahoo.com>
2021-07-21 17:53:38 +0300

    update on streams course example

--- src/com/sdaJavaAdvanced/streams/courseexample/Main.java
+++ src/com/sdaJavaAdvanced/streams/courseexample/Main.java
@@ -12,19 +12,20 @@ public class Main {
         List<String> courses = List.of(
                 "Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes"
         );
-
-        //courses.stream().forEach(course -> System.out.println(course));
-        courses.stream().filter(course -> course.contains("Spring"))
-                        .forEach(course -> System.out.println(course));
-        System.out.println("------------------------------------");
+        System.out.println("------------------al courses individually------------------");
+//- Print All Courses individually
         courses.stream().forEach(course -> System.out.println(course));
-        System.out.println("------------------------------------");
-        courses.stream().filter(word -> word.length()>4)
+        System.out.println("----------------courses that contain \"Spring\"--------------------");
+        //- Print Courses Containing the word "Spring"
+        courses.stream().filter(course -> course.contains("Spring"))
+                .forEach(course -> System.out.println(course));
+        System.out.println("----------------courses whose name has at least 4 letters--------------------");
+        courses.stream().filter(word -> word.length() >= 4)
                 .forEach(word -> System.out.println(word));
-        System.out.println("------------------------------------");
+        System.out.println("--------------number of characters in each course name----------------------");
         courses.stream().forEach(word -> System.out.println(word.length()));
-        System.out.println("------------------------------------");
-        courses.stream().sorted((word1,word2) -> Integer.compare(word2.length(),word1.length()))
-                        .findFirst().ifPresent(word -> System.out.println(word));
+        System.out.println("--------------course with the highest number of characters in his name----------------------");
+        courses.stream().sorted((word1, word2) -> Integer.compare(word2.length(), word1.length()))
+                .findFirst().ifPresent(word -> System.out.println(word));
     }
 }
