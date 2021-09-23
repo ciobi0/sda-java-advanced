48fbe3f95c868b8c237587ae0650b44ba87f0147
Cosmin Ciobanu <ciobanu.cosmin@yahoo.com>
2021-09-23 17:32:05 +0300

    update code for enum

--- src/com/enums/weekdays/EnumTest.java
+++ src/com/enums/weekdays/EnumTest.java
@@ -1,2 +1,37 @@
-package com.enums.weekdays;public class EnumTest {
+package com.enums.weekdays;
+
+public class EnumTest {
+    private Day day;
+
+    public EnumTest(Day day){
+        this.day = day;
+    }
+
+    public void tellItLikeItIs(){
+        switch (day) {
+            case MONDAY:
+                System.out.println("Mondays are bad.");
+                break;
+            case FRIDAY:
+                System.out.println("Fridays are better.");
+                break;
+            case SATURDAY:
+            case SUNDAY:
+                System.out.println("Weekends are the best.");
+                break;
+            default:
+                System.out.println("Midweek days are so-so.");
+        }
+    }
+
+    public static void main(String[] args) {
+        EnumTest firstDay = new EnumTest(Day.MONDAY);
+        firstDay.tellItLikeItIs();
+        EnumTest thirdDay = new EnumTest(Day.THURSDAY);
+        thirdDay.tellItLikeItIs();
+        EnumTest fifthDay = new EnumTest(Day.FRIDAY);
+        fifthDay.tellItLikeItIs();
+        EnumTest seventhDay = new EnumTest(Day.SUNDAY);
+        seventhDay.tellItLikeItIs();
+    }
 }
